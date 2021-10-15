87372571f738753f1eecf27449199a89973329d7
Franio Wywial <Franzciszek.Wywial@stud.it.htl-hl.ac.at>
2021-10-15 15:44:17 +0200

    bricks finish

--- src/main/java/at/htlhl/Brick.java
+++ src/main/java/at/htlhl/Brick.java
@@ -15,16 +15,56 @@ public class Brick {
 
     }
 
-    private int[] orangeRicky() {
+    private void orangeRicky() {
         this.brickMatrix = new int[]{   0, 0, 0, 0,
                                         0, 0, 0, 0,
                                         0, 0, 0, 1,
                                         0, 1, 1, 1};
-        return brickMatrix;
+
     }
 
-    private int[] blueRicky() {
-        this.brickMatrix = new int[] {}
+    private void blueRicky() {
+        this.brickMatrix = new int[] {  0, 0, 0, 0,
+                                        0, 0, 0, 0,
+                                        1, 0, 0, 0,
+                                        1, 1, 1, 0};
+
+        }
+    private void clevelandZ() {
+        this.brickMatrix = new int[] {  0, 0, 0, 0,
+                                        0, 0, 0, 0,
+                                        1, 1, 0, 0,
+                                        0, 1, 1, 0};
+
+    }
+    private void rhodeIslandZ() {
+        this.brickMatrix = new int[] {  0, 0, 0, 0,
+                                        0, 0, 0, 0,
+                                        0, 0, 1, 1,
+                                        0, 1, 1, 0};
+
+    }
+    private void hero() {
+        this.brickMatrix = new int[] {  0, 0, 0, 0,
+                                        0, 0, 0, 0,
+                                        0, 0, 0, 0,
+                                        1, 1, 1, 1};
+
+    }
+    private void teewee() {
+        this.brickMatrix = new int[] {  0, 0, 0, 0,
+                                        0, 0, 0, 0,
+                                        0, 1, 0, 0,
+                                        1, 1, 1, 0};
+
+    }
+    private void smashBoy() {
+        this.brickMatrix = new int[] {  0, 0, 0, 0,
+                                        0, 0, 0, 0,
+                                        1, 1, 0, 0,
+                                        1, 1, 0, 0};
+
+    }
     }
 
-}
+
