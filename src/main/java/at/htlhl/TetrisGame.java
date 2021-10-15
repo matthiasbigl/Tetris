c8c5cc1079fa1234bec106fa553f90fbfe85b5dc
Mario Toifl <mario.toifl@stud.it.htl-hl.ac.at>
2021-10-15 15:27:05 +0200

    Added Cell and TetrisGame

--- src/main/java/at/htlhl/TetrisGame.java
+++ src/main/java/at/htlhl/TetrisGame.java
@@ -1,6 +1,35 @@
 package at.htlhl;
 
+import javafx.scene.paint.Color;
+
 public class TetrisGame
 {
-
+    // Constants **************************************************************
+    public static final int GRID_WIDTH = 10;
+    public static final int GRID_HEIGHT = 20;
+    
+    // Fields *****************************************************************
+    private final Cell[][] grid;
+    
+    // Constructors ***********************************************************
+    public TetrisGame()
+    {
+        this.grid = new Cell[GRID_HEIGHT][GRID_WIDTH];
+        initGridMatrix();
+    }
+    
+    // Logic ******************************************************************
+    /**
+     * Fill the Grid Matrix with invisible Cells
+     */
+    private void initGridMatrix()
+    {
+        for (int y = 0; y < GRID_HEIGHT; y++)
+        {
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                grid[y][x] = new Cell();
+            }
+        }
+    }
 }
