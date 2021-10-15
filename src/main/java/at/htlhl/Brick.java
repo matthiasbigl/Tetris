c0e843b4677e0785c18b45e33f1c57a713e1a1f5
Jonas Tüchler <jonas.tuechler@gmail.com>
2021-10-15 15:44:59 +0200

    prettified

--- src/main/java/at/htlhl/Brick.java
+++ src/main/java/at/htlhl/Brick.java
@@ -3,10 +3,10 @@ package at.htlhl;
 public class Brick {
 
     /*
-            * * * *
-            * * * *
-            * * * *
-            * * * *
+     * * * *
+     * * * *
+     * * * *
+     * * * *
      */
 
     int[] brickMatrix;
@@ -16,55 +16,58 @@ public class Brick {
     }
 
     private void orangeRicky() {
-        this.brickMatrix = new int[]{   0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        0, 0, 0, 1,
-                                        0, 1, 1, 1};
-
+        this.brickMatrix = new int[]{0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 1,
+                0, 1, 1, 1};
     }
 
     private void blueRicky() {
-        this.brickMatrix = new int[] {  0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        1, 0, 0, 0,
-                                        1, 1, 1, 0};
+        this.brickMatrix = new int[]{0, 0, 0, 0,
+                0, 0, 0, 0,
+                1, 0, 0, 0,
+                1, 1, 1, 0};
 
-        }
+    }
+
     private void clevelandZ() {
-        this.brickMatrix = new int[] {  0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        1, 1, 0, 0,
-                                        0, 1, 1, 0};
+        this.brickMatrix = new int[]{0, 0, 0, 0,
+                0, 0, 0, 0,
+                1, 1, 0, 0,
+                0, 1, 1, 0};
 
     }
+
     private void rhodeIslandZ() {
-        this.brickMatrix = new int[] {  0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        0, 0, 1, 1,
-                                        0, 1, 1, 0};
+        this.brickMatrix = new int[]{0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 1, 1,
+                0, 1, 1, 0};
 
     }
+
     private void hero() {
-        this.brickMatrix = new int[] {  0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        1, 1, 1, 1};
+        this.brickMatrix = new int[]{0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                1, 1, 1, 1};
 
     }
+
     private void teewee() {
-        this.brickMatrix = new int[] {  0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        0, 1, 0, 0,
-                                        1, 1, 1, 0};
+        this.brickMatrix = new int[]{0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 1, 0, 0,
+                1, 1, 1, 0};
 
     }
+
     private void smashBoy() {
-        this.brickMatrix = new int[] {  0, 0, 0, 0,
-                                        0, 0, 0, 0,
-                                        1, 1, 0, 0,
-                                        1, 1, 0, 0};
-
+        this.brickMatrix = new int[]{0, 0, 0, 0,
+                0, 0, 0, 0,
+                1, 1, 0, 0,
+                1, 1, 0, 0};
     }
-    }
+}
 
 
