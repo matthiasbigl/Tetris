a3d9d6dab8a7e7393659f7882ac32887e2d5d248
Jonas Tüchler <jonas.tuechler@gmail.com>
2021-10-22 14:13:07 +0200

    fall console output working

--- src/main/java/at/htlhl/Fall.java
+++ src/main/java/at/htlhl/Fall.java
@@ -11,35 +11,43 @@ public class Fall {
 
     public void fall(TetrisController controller) {
         Cell[][] grid = TetrisGame.grid;
-        Color cellColor = Color.randomColor(false);
-        grid[0][0].setColor(cellColor);
+
+        Color red = new Color(1, 0, 0);
+        grid[0][4] = new Cell(red, true);
+        grid[0][5] = new Cell(red, true);
+        grid[1][4] = new Cell(red, true);
+        grid[2][4] = new Cell(red, true);
+
+        //Color cellColor = Color.randomColor(false);
+        //grid[0][0].setColor(cellColor);
         controller.updateGridMatrix(grid);
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                for (int y = 0; y < grid.length; y++) {
-                    if (!grid[y][0].getColor().equals(Color.TRANSPARENT)) {
-                        //System.out.println(1);
-                        grid[y][0].setColor(Color.TRANSPARENT);
-                        grid[y+1][0].setColor(cellColor);
-                        for (Cell x[] : grid) {
-                            for (Cell cell : x) {
-                                if (cell.getColor().equals(Color.TRANSPARENT)) {
-                                    System.out.print(0);
-                                } else {
-                                    System.out.print("x");
-                                }
-                            }
-                            System.out.println();
-                        }
-                        Platform.runLater(() -> controller.updateGridMatrix(grid));
 
-                        y = grid.length;
+                for (int y = grid.length - 1; y >= 0; y--) {
+                    for (int x = 0; x < grid[y].length; x++) {
+                        if (!grid[y][x].getColor().equals(Color.TRANSPARENT)) {
+                            grid[y][x].setColor(Color.TRANSPARENT);
+                            grid[y + 1][x].setColor(red);
+                            Platform.runLater(() -> controller.updateGridMatrix(grid));
+                        }
                     }
                 }
+                for (Cell x[] : grid) {
+                    for (Cell cell : x) {
+                        if (cell.getColor().equals(Color.TRANSPARENT)) {
+                            System.out.print("_");
+                        } else {
+                            System.out.print("X");
+                        }
+                    }
+                    System.out.println();
+                }
                 System.out.println();
+
             }
         }, 1000, 1000);
     }
