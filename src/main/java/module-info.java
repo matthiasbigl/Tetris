a862888dd1ae99c9b78b45afe844556875c79585
marilatte53 <marilatte53@gmx.at>
2021-11-18 17:12:04 +0100

    fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,10 @@ module at.htlhl {
     requires javafx.fxml;
 
     opens at.htlhl to javafx.fxml;
-    exports at.htlhl;
     exports at.htlhl.javafxtetris;
     opens at.htlhl.javafxtetris to javafx.fxml;
     exports at.htlhl.javafxtetris.grid;
     opens at.htlhl.javafxtetris.grid to javafx.fxml;
-    exports at.htlhl.javafxtetris.block;
-    opens at.htlhl.javafxtetris.block to javafx.fxml;
     exports at.htlhl.javafxtetris.graphics;
     opens at.htlhl.javafxtetris.graphics to javafx.fxml;
 }
