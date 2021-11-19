7aa5c3ea701ce156675b9685138a1e9a76789332
marilatte53 <marilatte53@gmx.at>
2021-11-19 12:30:59 +0100

    Refactor, added documentation & implemented Grid

--- src/main/java/at/htlhl/javafxtetris/graphics/TetrisController.java
+++ src/main/java/at/htlhl/javafxtetris/graphics/TetrisController.java
@@ -2,6 +2,7 @@ package at.htlhl.javafxtetris.graphics;
 
 import at.htlhl.javafxtetris.grid.Block;
 import at.htlhl.javafxtetris.grid.Cell;
+import at.htlhl.javafxtetris.grid.Grid;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
@@ -10,21 +11,93 @@ import javafx.scene.layout.Pane;
 public class TetrisController
 {
     // Fields *****************************************************************
-    @FXML
-    private GridPane tetrisGridPane;
-    @FXML
-    private GridPane previewGridPane;
-
-    private Pane[][] tetrisGrid;
-    private Pane[][] previewGrid;
+    @FXML private GridPane tetrisGridPane;
+    @FXML private GridPane previewGridPane;
+    private Pane[][] tetrisPaneMatrix;
+    private Pane[][] previewPaneMatrix;
 
     // Constructors ***********************************************************
     public TetrisController()
     {
     }
-    
+
+    // Logic ******************************************************************
+    // Init logic
     /**
-     * Updates a {@link Pane} object with the color from the given {@link Cell} object
+     * Initialises the tetrisGrid
+     *
+     * @param grid The data to fill the tetrisGrid with
+     */
+    public void initTetrisGrid(final Grid grid)
+    {
+        this.tetrisPaneMatrix = new Pane[grid.getHeight()][];
+        initGridPane(tetrisGridPane, tetrisPaneMatrix, grid);
+    }
+
+    /**
+     * Initialises the previewGrid
+     *
+     * @param initialBlock The data to fill the tetrisGrid with as a {@link Block}
+     */
+    public void initPreviewGrid(final Block initialBlock)
+    {
+        this.previewPaneMatrix = new Pane[4][4];
+        initGridPane(previewGridPane, previewPaneMatrix, initialBlock.toGrid());
+    }
+
+    /*
+     * Initialises the specified GridPane and paneMatrix using data from the cellMatrix
+     * The length of paneMatrix must be the same as cellMatrix
+     */
+    private void initGridPane(final GridPane gridPane, final Pane[][] paneMatrix, final Grid grid)
+    {
+        for(int y = 0; y < grid.getHeight(); y++)
+        {
+            paneMatrix[y] = new Pane[grid.getLine(y).length];
+            for(int x = 0; x < grid.getLine(y).length; x++)
+            {
+                paneMatrix[y][x] = new Pane();
+                gridPane.add(paneMatrix[y][x], x, y);
+            }
+        }
+        
+        updatePaneMatrix(paneMatrix, grid);
+    }
+
+    // Update logic
+    /**
+     * Updates the tetrisGrid using data from the specified {@link Grid}
+     *
+     * @param tetrisGrid The {@link Grid} that should be displayed
+     */
+    public void updateTetrisGrid(final Grid tetrisGrid)
+    {
+        updatePaneMatrix(tetrisPaneMatrix, tetrisGrid);
+    }
+
+    /**
+     * Updates the previewGrid using data from the specified {@link Block}
+     *
+     * @param newBlock The {@link Block} that should be displayed
+     */
+    public void updatePreviewGrid(Block newBlock)
+    {
+        updatePaneMatrix(previewPaneMatrix, newBlock.toGrid());
+    }
+    
+    private void updatePaneMatrix(final Pane[][] paneMatrix, final Grid grid)
+    {
+        for(int y = 0; y < grid.getHeight(); y++)
+        {
+            for(int x = 0; x < grid.getLine(y).length; x++)
+            {
+                updatePane(paneMatrix[y][x], grid.getVisibleCell(x, y));
+            }
+        }
+    }
+
+    /**
+     * Updates a {@link Pane} object using the data from the specified {@link Cell} object
      *
      * @param cell The {@link Cell} object representing a tetris tile
      */
@@ -36,81 +109,19 @@ public class TetrisController
             pane.setVisible(cell.isSolid());
         });
     }
-    
-    /**
-     * Initialises the objects in the GridPane
-     *
-     * @param initialGrid
-     */
-    public void initGridMatrix(final Cell[][] initialGrid)
-    {
-        this.tetrisGrid = new Pane[initialGrid.length][];
-        
-        for (int y = 0; y < initialGrid.length; y++)
-        {
-            this.tetrisGrid[y] = new Pane[initialGrid[y].length];
-            for (int x = 0; x < initialGrid[y].length; x++)
-            {
-                this.tetrisGrid[y][x] = new Pane();
-                tetrisGridPane.add(tetrisGrid[y][x], x, y);
-            }
-        }
-    }
-    
-    /**
-     * Updates the GridPane using the given grid data
-     *
-     * @param newGrid The grid that should be displayed. Must be the same size as the GridPane
-     */
-    public void updateTetrisGrid(final Cell[][] newGrid)
-    {
-        for (int y = 0; y < tetrisGrid.length; y++)
-        {
-            for (int x = 0; x < tetrisGrid[y].length; x++)
-            {
-                updatePane(tetrisGrid[y][x], newGrid[y][x]);
-            }
-        }
-    }
-    
+
     public void addCellsToTetrisGrid(final int startX, final int startY, final Cell[][] newCells)
     {
-        for (int y = 0; y < newCells.length && (startY + y) < tetrisGrid.length; y++)
+        for(int y = 0; y < newCells.length && (startY + y) < tetrisPaneMatrix.length; y++)
         {
-            for (int x = 0; x < newCells[y].length && (startX + x) < tetrisGrid[y].length; x++)
+            for(int x = 0; x < newCells[y].length && (startX + x) < tetrisPaneMatrix[y].length; x++)
             {
                 final int actualY = startY + y;
                 final int actualX = startX + x;
                 if(newCells[y][x].isSolid())
                 {
-                    updatePane(tetrisGrid[actualY][actualX], newCells[y][x]);
+                    updatePane(tetrisPaneMatrix[actualY][actualX], newCells[y][x]);
                 }
-            }
-        }
-    }
-
-    public void initPreviewGrid()
-    {
-        this.previewGrid = new Pane[4][4];
-
-        for (int y = 0; y < previewGrid.length; y++)
-        {
-            for (int x = 0; x < previewGrid[y].length; x++)
-            {
-                this.previewGrid[y][x] = new Pane();
-                previewGridPane.add(previewGrid[y][x], x, y);
-            }
-        }
-    }
-
-    public void updatePreviewGrid(Block newBlock)
-    {
-        Cell[][] newGrid = newBlock.toCellMatrix();
-        for (int y = 0; y < newGrid.length; y++)
-        {
-            for (int x = 0; x < newGrid[y].length; x++)
-            {
-                updatePane(previewGrid[y][x], newGrid[y][x]);
             }
         }
     }
