3ba1d06a46037c01e7c4819731d1332031fc129f
marilatte53 <marilatte53@gmx.at>
2021-12-05 21:31:01 +0100

    Refactor & added stats

--- src/main/java/at/htlhl/javafxtetris/TetrisGame.java
+++ src/main/java/at/htlhl/javafxtetris/TetrisGame.java
@@ -7,6 +7,7 @@ import at.htlhl.javafxtetris.grid.block.Block;
 import at.htlhl.javafxtetris.grid.block.Direction;
 import at.htlhl.javafxtetris.grid.block.FallingBlock;
 import at.htlhl.javafxtetris.grid.TetrisGrid;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.Scene;
 
 import java.util.Timer;
@@ -17,53 +18,62 @@ public class TetrisGame
     // Constants **************************************************************
     public static final int GRID_WIDTH = 10;
     public static final int GRID_HEIGHT = 20;
-
+    
     // Fields *****************************************************************
     // Grid and Blocks
     private final TetrisController controller;
     private final TetrisGrid tetrisGrid;
     private Block nextBlock;
-
+    
     // Timer & Tick loop
     private Timer tickTimer;
     private boolean isRunning;
     private boolean isPaused;
-
-    // Movement
     private long totalTickCount; // The total number of ticks that have happened
     private long lastBlockFall;  // The last tick the FallingBlock was moved
-
+    
+    // Stats
+    private SimpleIntegerProperty levelProp;        // The level the player is in (10 lines = 1 level)
+    private SimpleIntegerProperty linesClearedProp; // The total number of lines the player has cleared
+    private SimpleIntegerProperty scoreProp;
+    
     // Constructors ***********************************************************
     public TetrisGame(TetrisController controller, Scene scene)
     {
-        processUserInput(scene);
-
-        this.controller = controller;
-
         // Init loop variables
         this.isRunning = false;
         this.isPaused = true;
-    
+        
+        // Init stats
+        this.levelProp = new SimpleIntegerProperty(1);
+        this.linesClearedProp = new SimpleIntegerProperty();
+        this.scoreProp = new SimpleIntegerProperty();
+        
         // Init the grid
         this.nextBlock = Block.randomBlock();
         this.tetrisGrid = new TetrisGrid(GRID_WIDTH, GRID_HEIGHT, nextBlock);
         
+        // User input
+        initUserInput(scene);
+        
         // Init the controller
+        this.controller = controller;
         controller.initTetrisGrid(tetrisGrid);
         controller.initPreviewGrid(nextBlock.getDefaultState().toGrid());
-    
+        controller.initStats(this);
+        
         generateNextBlock();
     }
-
-    // Logic ******************************************************************
+    
+    // Game loop **************************************************************
     public void start()
     {
         if(isRunning())
             return;
-    
+        
         this.isRunning = true;
         unpause();
-    
+        
         this.totalTickCount = 1;
         this.tickTimer = new Timer();
         tickTimer.scheduleAtFixedRate(new TimerTask()
@@ -78,28 +88,28 @@ public class TetrisGame
             }
         }, 1000, 10);
     }
-
+    
     public void stop()
     {
         if(!isRunning())
             return;
-    
+        
         this.isRunning = false;
         pause();
         
         tickTimer.cancel();
     }
-
+    
     public void pause()
     {
         this.isPaused = true;
     }
-
+    
     public void unpause()
     {
         this.isPaused = false;
     }
-
+    
     /**
      * @return Whether the game is running
      */
@@ -107,7 +117,7 @@ public class TetrisGame
     {
         return isRunning;
     }
-
+    
     /**
      * @return Whether the game is paused
      */
@@ -115,22 +125,25 @@ public class TetrisGame
     {
         return isPaused;
     }
-
+    
+    // Tick *******************************************************************
+    
     /**
      * Is called x times per second
      */
     private void tick()
     {
+        // TODO: increase tick speed depending on level
         if(lastBlockFall + 30 <= totalTickCount)
         {
             this.lastBlockFall = totalTickCount;
-    
+            
             if(!tetrisGrid.getFallingBlock().tryMove(tetrisGrid, Direction.DOWN))
             {
                 placeCurrentBlock();
             }
         }
-
+        
         totalTickCount++;
         controller.updateTetrisGrid(tetrisGrid);
         controller.updateFallingBlock(tetrisGrid.getFallingBlock());
@@ -140,29 +153,22 @@ public class TetrisGame
     {
         this.lastBlockFall = totalTickCount;
         final FallingBlock fallingBlock = tetrisGrid.getFallingBlock();
-        if(fallingBlock.canPlaceBlockIn(tetrisGrid))
+        if(!fallingBlock.tryPlaceBlockIn(tetrisGrid))
         {
-            // If the Block can be placed, place it in the Grid
-            fallingBlock.placeBlockIn(tetrisGrid);
-        }
-        else
-        {
-            // Otherwise the player has lost
             this.stop();
             App.instance().showLosingScreen();
             return;
         }
-    
+        
         // Update the Falling Block in the Grid
         tetrisGrid.setFallingBlock(nextBlock);
         // Generate a new Block
         generateNextBlock();
         // Delete all full lines in the Grid
-        tetrisGrid.deleteFullLines();
-        // Update the points and lines
-        controller.updatePointsAndLines();
+        final int deletedLines = tetrisGrid.deleteFullLines();
+        updateStats(deletedLines);
     }
-
+    
     /*
      * Generates a new Block that is different from nextBlock
      */
@@ -171,33 +177,77 @@ public class TetrisGame
         this.nextBlock = Block.values()[(nextBlock.ordinal() +
                 (int) (Math.random() * (Block.values().length - 1)) + 1)
                 % Block.values().length];
+        
         // Update the preview Block in the controller
-        controller.updatePreviewGrid(nextBlock.getDefaultState().toGrid());
+        controller.updatePreview(nextBlock.getDefaultState());
     }
-
-    private void processUserInput(Scene scene)
+    
+    /**
+     * Method to add Points, Level and lines based on the amount of lines removed
+     *
+     * @param amountOfLines The amount of lines cleared
+     */
+    private void updateStats(int amountOfLines)
     {
-        scene.setOnKeyPressed(e -> {
+        // TODO: set score and lines to 0 if lost
+        
+        // Add amount of lines deleted to lines
+        // .add() creates a new property and DOES NOT change the value
+        linesClearedProp.set(linesClearedProp.get() + amountOfLines);
+        
+        // add points to score depending on level
+        // using the scoring system from https://tetris.fandom.com/wiki/Scoring
+        int baseScore;
+        switch(amountOfLines)
+        {
+            case 1:
+                baseScore = 40;
+                break;
+            case 2:
+                baseScore = 100;
+                break;
+            case 3:
+                baseScore = 300;
+                break;
+            case 4:
+                baseScore = 1200;
+                break;
+            default:
+                baseScore = 0;
+                break;
+        }
+        
+        scoreProp.set(scoreProp.get() + (baseScore * levelProp.get()));
+        
+        // for every 10 lines removed increase the level by one
+        levelProp.set(linesClearedProp.get() / 10 + 1);
+    }
+    
+    private void initUserInput(Scene scene)
+    {
+        scene.setOnKeyPressed(e ->
+        {
             FallingBlock fallingBlock = tetrisGrid.getFallingBlock();
-            switch (e.getCode().getCode()) {
+            switch(e.getCode().getCode())
+            {
                 //A, arrow_left
                 case 65:
                 case 37:
                     fallingBlock.tryMove(tetrisGrid, Direction.LEFT);
                     break;
-
+                
                 // S, arrow_down
                 case 83:
                 case 40:
                     fallingBlock.tryMove(tetrisGrid, Direction.DOWN);
                     break;
-
+                
                 // D, arrow_right
                 case 68:
                 case 39:
                     fallingBlock.tryMove(tetrisGrid, Direction.RIGHT);
                     break;
-
+                
                 // space
                 case 32:
                     // Move the block down one by one
@@ -205,27 +255,27 @@ public class TetrisGame
                     final Grid cellsToPlace = fallingBlock.getBlockState().toGrid();
                     int lowestValidY = 0;
                     
-                    for (int y = 0; y < tetrisGrid.getHeight() - fallingBlock.getY(); y++)
+                    for(int y = 0; y < tetrisGrid.getHeight() - fallingBlock.getY(); y++)
                     {
-                        if (tetrisGrid.canPlace(cellsToPlace, fallingBlock.getX(), fallingBlock.getY() + y))
+                        if(tetrisGrid.canPlace(cellsToPlace, fallingBlock.getX(), fallingBlock.getY() + y))
                             lowestValidY = y;
                         else
                             break;
                     }
-    
+                    
                     fallingBlock.setY(fallingBlock.getY() + lowestValidY);
                     placeCurrentBlock();
                     
                     break;
-
+                
                 // Q
                 case 81:
                     break;
-
+                
                 // W
                 case 87:
                     break;
-
+                
                 // E
                 case 69:
                     break;
@@ -233,5 +283,21 @@ public class TetrisGame
                     break;
             }
         });
+    }
+    
+    // Properties *************************************************************
+    public SimpleIntegerProperty scoreProperty()
+    {
+        return scoreProp;
+    }
+    
+    public SimpleIntegerProperty levelProperty()
+    {
+        return levelProp;
+    }
+    
+    public SimpleIntegerProperty linesClearedProperty()
+    {
+        return linesClearedProp;
     }
 }
