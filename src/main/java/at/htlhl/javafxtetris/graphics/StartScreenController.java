d6c23c8dce8ad1db0235f1f94e67ea4e97bd83e9
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-02-04 13:25:06 +0100

    COntroller hinzugefügt

--- src/main/java/at/htlhl/javafxtetris/graphics/StartScreenController.java
+++ src/main/java/at/htlhl/javafxtetris/graphics/StartScreenController.java
@@ -1,8 +1,14 @@
 package at.htlhl.javafxtetris.graphics;
 
+import at.htlhl.javafxtetris.App;
 import at.htlhl.javafxtetris.TetrisGame;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 public class StartScreenController {
 
@@ -14,6 +20,25 @@ public class StartScreenController {
 
     @FXML
     private Button blitz;
+
+    private TetrisController controller;
+    private Scene scene;
+
+    private void init(){
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("TetrisView.fxml"));
+
+        Pane root;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        controller = loader.getController();
+         scene = new Scene(root);
+    }
+
 
     public void StartClassic() {
         TetrisGame game = new TetrisGame(controller, scene);
