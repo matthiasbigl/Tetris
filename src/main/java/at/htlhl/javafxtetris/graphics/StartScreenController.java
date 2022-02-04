a97be2abc189859cf31d3505c0accb31cb008064
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-02-04 13:21:32 +0100

    COntroller hinzugefügt

--- src/main/java/at/htlhl/javafxtetris/graphics/StartScreenController.java
+++ src/main/java/at/htlhl/javafxtetris/graphics/StartScreenController.java
@@ -1,5 +1,6 @@
 package at.htlhl.javafxtetris.graphics;
 
+import at.htlhl.javafxtetris.TetrisGame;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -15,14 +16,36 @@ public class StartScreenController {
     private Button blitz;
 
     public void StartClassic() {
+        TetrisGame game = new TetrisGame(controller, scene);
+        primaryStage.setOnCloseRequest(e -> game.stop());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.setFullScreen(true);
+
+        game.start();
 
     }
 
     public void StartBlitz() {
+        TetrisGame game = new TetrisGame(controller, scene);
+        primaryStage.setOnCloseRequest(e -> game.stop());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.setFullScreen(true);
+        BlitzGame blitz = new BlitzGame(game, primaryStage);
+        blitz.startBlitzMode();
 
     }
 
     public void StartFourty() {
+        FourtyLineGame game = new FourtyLineGame(controller,scene);
+        primaryStage.setOnCloseRequest(e -> game.stop());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.setFullScreen(true);
+
+        game.start();
+
 
     }
 
