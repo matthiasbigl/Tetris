3ef3d65353d88fc2361351eb3266ab1d4a14b568
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-02-04 13:18:52 +0100

    COntroller hinzugefügt

---
+++ src/main/java/at/htlhl/javafxtetris/graphics/StartScreenController.java
@@ -0,0 +1,30 @@
+package at.htlhl.javafxtetris.graphics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartScreenController {
+
+    @FXML
+    private Button classic;
+
+    @FXML
+    private Button fourtyLines;
+
+    @FXML
+    private Button blitz;
+
+    public void StartClassic() {
+
+    }
+
+    public void StartBlitz() {
+
+    }
+
+    public void StartFourty() {
+
+    }
+
+
+}
